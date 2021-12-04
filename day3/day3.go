be5cb54f464aa240530834cbeae8de54714f199d
Mederic de Verdilhac <med2ver@gmail.com>
2021-12-04 13:31:46 +0100

    Finish day3

--- day3/day3.go
+++ day3/day3.go
@@ -35,10 +35,11 @@ func main() {
 	// filePath := "day3/day3_input.test.txt"
 	filePath := "day3/day3_input.txt"
 	fileContent := readFile(filePath)
-	fmt.Println("Part1 result : ", step1_calculateGamma(fileContent))
-	// fmt.Println("Part2 result : ", step2_followInstructionsWithAim(fileContent))
+	fmt.Println("Part1 result : ", step1_calculateGammaAndEpsilon(fileContent))
+	fmt.Println("Part2 result : ", step2_calculateLifeSupportRating(fileContent))
 }
 
+// get each line as a list of integer
 func parseLineAsBits(line string) []int {
 	var bits []int
 	for _, bitStr := range line {
@@ -47,29 +48,38 @@ func parseLineAsBits(line string) []int {
 	return bits
 }
 
+func parseWholeInputAs2dArray(input []string) [][]int {
+	var table [][]int
+	for _, line := range input {
+		table = append(table, parseLineAsBits(line))
+	}
+	return table
+}
+
 // https://stackoverflow.com/questions/21322173/convert-rune-to-int
 func convertRuneToInt(rune rune) int {
 	return int(rune - '0')
 }
 
-func sumBits(input []string) []int {
-
-	inputLength := len(input[0])
-	result := make([]int, inputLength)
-
-	for _, line := range input {
-		bits := parseLineAsBits(line)
-		for idx, bit := range bits {
-			result[idx] += bit
-		}
+func convertBinstrToInt(ListofBits []int) (string, int) {
+	resultStr := ""
+	for _, bit := range ListofBits {
+		resultStr = fmt.Sprint(resultStr, bit)
 	}
-	return result
+	result, _ := strconv.ParseInt(resultStr, 2, 64)
+	return resultStr, int(result)
 }
 
-func getBits(input []string) ([]int, []int) {
-	// get the sum of each digits
-	sums := sumBits(input)
-	fmt.Println(sums)
+func step1_calculateGammaAndEpsilon(input []string) string {
+	// get input as int table
+	table := parseWholeInputAs2dArray(input)
+	// get the sum of each digits column
+	sums := make([]int, len(table[0]))
+	for raw := 0; raw < len(table); raw++ {
+		for idx, bit := range table[raw] {
+			sums[idx] += bit
+		}
+	}
 
 	// then get each digit of gamma
 	// if sum of all bits is more than half of total bit count, there is a majority of 1
@@ -85,22 +95,80 @@ func getBits(input []string) ([]int, []int) {
 			epsilonBits[idx] = 1
 		}
 	}
-	return gammaBits, epsilonBits
+
+	gammaStr, gammaInt := convertBinstrToInt(gammaBits)
+	epsilonStr, epsilonInt := convertBinstrToInt(epsilonBits)
+
+	return fmt.Sprintf("Gamma: %s/%d ;  Epsilon: %s/%d ; result %d", gammaStr, gammaInt, epsilonStr, epsilonInt, gammaInt*epsilonInt)
 }
 
-func step1_calculateGamma(input []string) string {
-	gammaBits, epsilonBits := getBits(input)
-	gammaStr := ""
-	epsilonStr := ""
-	for _, bit := range gammaBits {
-		gammaStr = fmt.Sprint(gammaStr, bit)
-	}
-	for _, bit := range epsilonBits {
-		epsilonStr = fmt.Sprint(epsilonStr, bit)
+//################################################################################
+
+func step2_calculateLifeSupportRating(input []string) string {
+	// get input as int table
+	table := parseWholeInputAs2dArray(input)
+	columnCount := len(table[0])
+
+	o2table := table
+	for index := 0; index < columnCount; index++ {
+		o2table = filterTableBasedOnMajoritaryColumnBit(o2table, index)
 	}
 
-	gammaInt, _ := strconv.ParseInt(gammaStr, 2, 64)
-	epsilonInt, _ := strconv.ParseInt(epsilonStr, 2, 64)
+	co2table := table
+	for index := 0; index < columnCount; index++ {
+		co2table = filterTableBasedOnMinorityColumnBit(co2table, index)
+		if len(co2table) == 1 {
+			break
+		}
+	}
 
-	return fmt.Sprintf("Gamma:%s / %d ;  Epsilon: %s / %d ; result %d", gammaStr, gammaInt, epsilonStr, epsilonInt, gammaInt*epsilonInt)
+	// convert results
+	o2GenRatingStr, o2GenRating := convertBinstrToInt(o2table[0])
+	co2GenRatingStr, co2GenRating := convertBinstrToInt(co2table[0])
+
+	return fmt.Sprintf("O2: %s/%d; CO2: %s/%d ; result %d", o2GenRatingStr, o2GenRating, co2GenRatingStr, co2GenRating, o2GenRating*co2GenRating)
+}
+
+func filterTableBasedOnMajoritaryColumnBit(table [][]int, filteringColumn int) [][]int {
+	// get the majoritary bit of the selected column
+	sum := 0
+	for raw := 0; raw < len(table); raw++ {
+		sum += table[raw][filteringColumn]
+	}
+
+	majorityBit := 1
+	if sum*2 < len(table) {
+		majorityBit = 0
+	}
+
+	// filter out any member of the table which doesn't have the majority bit
+	var result [][]int
+	for _, value := range table {
+		if value[filteringColumn] == majorityBit {
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func filterTableBasedOnMinorityColumnBit(table [][]int, filteringColumn int) [][]int {
+	// get the majoritary bit of the selected column
+	sum := 0
+	for raw := 0; raw < len(table); raw++ {
+		sum += table[raw][filteringColumn]
+	}
+
+	minorityBit := 0
+	if sum*2 < len(table) {
+		minorityBit = 1
+	}
+
+	// filter out any member of the table which doesn't have the majority bit
+	var result [][]int
+	for _, value := range table {
+		if value[filteringColumn] == minorityBit {
+			result = append(result, value)
+		}
+	}
+	return result
 }
