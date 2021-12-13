40fb4764f5fb0511464804a17135640b9e382286
Mederic de Verdilhac <mederic.deverdilhac@orange.com>
2021-12-13 16:35:51 +0100

    Update run all script

--- run_all.sh
+++ run_all.sh
@@ -4,16 +4,28 @@ set -euo pipefail
 
 DAY_LIST=$(ls -1 -d day*/ )
 
+start=`date +%s.%N`
+
+
+
 for day in $DAY_LIST
 do
     echo $day
     go build ${day%/}/main/*.go
 done
+copil_end=`date +%s.%N`
+
 
 for day in $DAY_LIST
 do
-    time ./run_${day%/}
+    ./run_${day%/}
 done
+end=`date +%s.%N`
+
+echo
+echo "Compilation time: $( echo "$copil_end - $start" | bc -l )s"
+echo "Execution time: $( echo "$end - $copil_end" | bc -l )s"
+echo "Total run time: $( echo "$end - $start" | bc -l )s"
 
 # Remove built packages
 rm -f run_day*
